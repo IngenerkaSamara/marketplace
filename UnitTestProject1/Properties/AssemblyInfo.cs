4ae1ee0529ad14086c03163cc9f368306d4d6bb1
mabramovsd <beavisabra@yandex.ru>
2021-09-09 15:23:34 +0400

    Another test

---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a7e75c2e-dbdc-4939-b64f-2b0f51c4e55c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using FormsAppEvoX;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Game game1 = new Game { name = "Game 1", price = 20 };
+            Form1.games_list = new List<Game> { game1 };
+
+            GameForm gf = new GameForm(game1.name);
+
+            Assert.AreEqual(game1.name, gf.game1.name);
+            Assert.AreEqual(game1.price, gf.game1.price);
+        }
+    }
+}
